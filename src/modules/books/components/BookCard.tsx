d849a7070be1e3efa6d5bdd51f6986984063b67e
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-10 17:28:27 +0800

    feat: implemented execise two of stage 2.

--- src/modules/books/components/BookCard.tsx
+++ src/modules/books/components/BookCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardContent, Stack, Typography } from '@mui/material';
 import type { JSX } from 'react';
 
 import type { Book } from '../types';
+import { BookStatusBadge } from './BookStatusBadge';
 
 interface BookCardProps {
   book: Book;
@@ -21,11 +22,19 @@ interface BookCardProps {
  * Lay them out with <Stack spacing={1}>.
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function BookCard(_props: BookCardProps): JSX.Element {
+export function BookCard({ book }: BookCardProps): JSX.Element {
   // TODO (exercise 2):
   //   - Import BookStatusBadge from './BookStatusBadge'.
   //   - Build the layout described above.
   // Rename `_props` to `{ book }` once you use it.
   void Card; void CardContent; void Stack; void Typography;
+  <Card>
+    <CardContent>
+      <Typography variant="h6">{book.title}</Typography>
+      <Typography variant="body2" sx={{ color: 'text.secondary' }}>{book.author}</Typography>
+      <Typography variant="caption" sx={{ color: 'text.secondary' }}>{book.pages}</Typography>
+      <BookStatusBadge status={book.status}></BookStatusBadge>
+    </CardContent>
+  </Card>
   throw new Error('Not implemented');
 }
