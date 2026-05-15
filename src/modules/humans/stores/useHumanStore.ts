1ec962e7222977bcb6351bc8cc29c6cc3a44f0f1
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-05-15 19:26:44 +0800

    feat: added feature.

--- src/modules/humans/stores/useHumanStore.ts
+++ src/modules/humans/stores/useHumanStore.ts
@@ -1,6 +1,12 @@
 import { create } from '../../../storeRegistry';
-import type { Human } from '../types';
+import { useActionSlice, type ActionSlice } from '../slices/actionSlice';
 
-export interface HumanStore {
+export type HumanStore = ActionSlice & {
   reset: () => void
-}
+};
+
+export const useHumanStore = create<HumanStore>((set, get, ...rest) => ({
+   ...useActionSlice()(set, get, ...rest),
+
+   reset: () => set({ humans: [], loading: false }),
+}));
